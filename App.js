a8fb1e63b300f921c2e3492fe526e55855137f70
Henna <henna.honkanen1@gmail.com>
2021-03-07 06:05:31 +0200

    first commit

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log("Hello!");
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
